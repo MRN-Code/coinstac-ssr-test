23194cb556e0dd9e61f417e501bbcd9b07f6c061
tkah <tkahleck@unm.edu>
2018-04-02 13:24:59 -0600

    updating parser to take updated spec

--- parsers.py
+++ parsers.py
@@ -15,7 +15,7 @@ def parse_for_each_y(args, X_files, y_files, dependent):
         if file.split('-')[-1] in X_files:
             with open(
                     os.path.join(args["state"]["baseDirectory"],
-                                 file.split('-')[-1])) as fh:
+                                 file)) as fh:
                 for line in fh:
                     if line.startswith(dependent[0]):
                         y.append(float(line.split('\t')[1]))
@@ -53,11 +53,11 @@ def fsl_parser(args):
     ys = X_types
     result = [x for x, y in zip(xs, ys) if y == 'boolean']
     pd.options.mode.chained_assignment = None  # default='warn'
-    X[result] = (X[result] == 'True').astype(int)
+    X[result] = (X[result] == True).astype(int)
     X = X.apply(pd.to_numeric, errors='ignore')
 
-    y_files = y_info[0][0]
-    y_labels = y_info[1][0]
+    y_files = y_info[0]
+    y_labels = y_info[1]
 
     y_list = parse_for_y_array(args, X_files, y_files, y_labels)
     y = pd.DataFrame.from_records(y_list, columns=y_labels)
